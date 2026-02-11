0d305bb5820abdfb592f19a6a0f03bf1ee748d9a
uitspitss <3781626+uitspitss@users.noreply.github.com>
2026-02-11 11:34:06 +0900

    feat: add shadcn-ui

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -60,6 +60,7 @@ export const routeTree = rootRouteImport
 
 import type { getRouter } from './router.tsx'
 import type { startInstance } from './start.ts'
+
 declare module '@tanstack/react-start' {
   interface Register {
     ssr: true
