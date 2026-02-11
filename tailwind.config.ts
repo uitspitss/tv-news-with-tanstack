f1ebc767fdb76127409f3526f5f9946bc229b845
uitspitss <3781626+uitspitss@users.noreply.github.com>
2026-02-11 11:34:06 +0900

    feat: add shadcn-ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,17 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default {
-  darkMode: ['class'],
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
+  darkMode: ["class"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       colors: {},
     },
   },
-  plugins: [require('tailwindcss-animate')],
-} satisfies Config
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
